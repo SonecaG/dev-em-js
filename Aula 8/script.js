cc043c3f6c1727f41d95583435de8b04fef8f57e
SonecaG <jcalvimboy@gmail.com>
2025-08-07 17:51:08 -0300

    Add files via upload

---
+++ Aula 8/script.js
@@ -0,0 +1,41 @@
+
+ale = parseInt(Math.random()*21)
+
+console.log(ale)
+// prompt()
+let algo = prompt('Digite algo')
+
+if (algo == 20){
+
+console.log('O numero é ', algo)
+
+}else{
+
+console.log('è o numero', algo)
+
+}
+
+
+
+// if ternario 
+idade = 18
+
+// let  teste  = idade>=18? 'maior':'menor'
+
+// console.log(teste)
+
+
+
+// switch(idade){
+
+// case 18:
+//     console.log('maior de idade')
+//     break
+// case 17:
+//     console.log('menor de idade')
+//     break
+// default:
+//     console.log('Indefinido')    
+// }
+
+
